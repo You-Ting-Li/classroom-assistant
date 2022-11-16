8d194fc6c55ca625ce61270bd202b7e1fcb6958b
UT0903 <b07902048@ntu.edu.tw>
2022-11-16 19:59:28 +0800

    fix to apply a timer to decide when to clone

--- app/routes/confirm/containers/ActionableConfirmFooter.jsx
+++ app/routes/confirm/containers/ActionableConfirmFooter.jsx
@@ -1,17 +1,34 @@
 import { push } from "react-router-redux"
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 
 import { Link } from "react-router"
 import { connect } from "react-redux"
 import { submissionCloneAll } from "../../../modules/submissions/actions/submission-clone-all"
-
 import Footer from "../../shared/components/Footer"
 
 const ConfirmFooter = ({
   confirmClickHandler
 }) => {
+  const [disable, setDisable] = useState(false);
+  const deadline = new Date(2022, 10, 19, 0, 1, 0); //FIX_TIME_HERE
+  const [curDate, setCurDate] = useState(new Date());
+  const cloneall = () => {
+    confirmClickHandler();
+  }
+  useEffect(()=>{
+    const interval_t = deadline.getTime() - curDate.getTime();
+    let timer;
+    if (interval_t > 0){
+      timer = setTimeout(cloneall
+      , interval_t);
+    }
+    return () => clearTimeout(timer);
+  }, [])
+  
+  console.log('get_time1 %s %s', deadline, curDate)
+  console.log('get_time2 %d %d', deadline.getTime(), curDate.getTime())
   return (
     <Footer>
       <Link to="/select" key={0}>
@@ -19,7 +36,10 @@ const ConfirmFooter = ({
       </Link>
       <button
         className="btn btn-success pull-right"
-        onClick={confirmClickHandler}
+        onClick={() => {
+          setDisable(true);
+        }}
+        disabled={disable}
       >
         Archive
       </button>
@@ -33,8 +53,8 @@ ConfirmFooter.propTypes = {
 
 const mapDispatchToProps = (dispatch) => ({
   confirmClickHandler: () => {
-    dispatch(push("/archive"))
-    dispatch(submissionCloneAll())
+    dispatch(push("/archive"));
+    dispatch(submissionCloneAll());
   }
 })
 
